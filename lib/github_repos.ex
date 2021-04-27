a947a856253541b7ae5a18d206e0e9398a07c0a0
Alesandro Silva <silva_alesandro@outlook.com>
2021-04-27 10:08:50 -0400

    feat: add create and get user to facade

--- lib/github_repos.ex
+++ lib/github_repos.ex
@@ -1,4 +1,10 @@
 defmodule GithubRepos do
+  alias GithubRepos.Users.{Create, Get}
   alias GithubRepos.Repositories.Client
+
   defdelegate get_repos(user_name), to: Client, as: :get_repos
+
+  defdelegate create_user(params), to: Create, as: :call
+
+  defdelegate get_user(id), to: Get, as: :call
 end
