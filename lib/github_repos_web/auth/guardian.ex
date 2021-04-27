8f4cc43cb7ceff12ea8f14b06cf1a0cbfcb581a7
Alesandro Silva <silva_alesandro@outlook.com>
2021-04-27 12:19:07 -0400

    feat: add guardian auth module

---
+++ lib/github_repos_web/auth/guardian.ex
@@ -0,0 +1,13 @@
+defmodule GithubRepos.Auth.Guardian do
+  use Guardian, otp_app: :github_repos
+
+  alias GithubRepos.User
+
+  def subject_for_token(%User{id: id}, _claims), do: {:ok, id}
+
+  def resource_from_claims(claims) do
+    claims
+    |> Map.get("sub")
+    |> GithubRepos.get_user()
+  end
+end
